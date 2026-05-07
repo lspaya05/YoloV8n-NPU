616df6f2425bdd14db165f8ae3d70ed02b22eb08
kingsmanbl <bernardolinbl@gmail.com>
2026-05-07 16:26:52 -0700

    Convert VPE datapath to INT8

--- src/VectorProcessingUnit/vpe.sv
+++ src/VectorProcessingUnit/vpe.sv
@@ -1,6 +1,6 @@
 // Name: Leonard Paya, Bernardo Lin
 // Date: 2026-04-27
-// Vector Processing Element (VPE).
+// This is the Vector Processing Element (VPE).
 //
 // Datapath matches the VPE block diagram:
 //     - left operand MUX selects either the lane input or Data-H from the right neighbor
@@ -18,25 +18,25 @@ module vpe (
     input  logic        output_mux_select,
     input  logic        register_enable,
     input  logic [2:0]  fu_opcode,
-    input  logic [31:0] in_a,
-    input  logic [31:0] in_b,
-    input  logic [31:0] data_h_in,
-    output logic [31:0] out
+    input  logic [7:0]  in_a,
+    input  logic [7:0]  in_b,
+    input  logic [7:0]  data_h_in,
+    output logic [7:0]  out
 );
 
-    logic [31:0] left_operand;
-    logic [31:0] right_operand;
-    logic [31:0] fu_out;
-    logic [31:0] register_out;
+    logic [7:0] left_operand;
+    logic [7:0] right_operand;
+    logic [7:0] fu_out;
+    logic [7:0] register_out;
 
-    mux2to1 left_operand_mux (
+    mux2to1 #(.BIT_WIDTH(8)) left_operand_mux (
         .d0(in_a),
         .d1(data_h_in),
         .select(left_mux_select),
         .y(left_operand)
     );
 
-    mux2to1 right_operand_mux (
+    mux2to1 #(.BIT_WIDTH(8)) right_operand_mux (
         .d0(in_b),
         .d1(register_out),
         .select(right_mux_select),
@@ -50,7 +50,7 @@ module vpe (
         .opcode(fu_opcode)
     );
 
-    vpe_reg local_register (
+    vpe_reg #(.BIT_WIDTH(8)) local_register (
         .in(fu_out),
         .out(register_out),
         .clk(clk),
@@ -58,7 +58,7 @@ module vpe (
         .rst(rst)
     );
 
-    mux2to1 output_mux (
+    mux2to1 #(.BIT_WIDTH(8)) output_mux (
         .d0(fu_out),
         .d1(register_out),
         .select(output_mux_select),
