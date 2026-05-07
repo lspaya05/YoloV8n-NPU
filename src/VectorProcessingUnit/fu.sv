e6cb3ca2ffe2edb062023d22b06bbea1c546b332
kingsmanbl <bernardolinbl@gmail.com>
2026-05-07 16:26:52 -0700

    Convert VPU FU to INT8

--- src/VectorProcessingUnit/fu.sv
+++ src/VectorProcessingUnit/fu.sv
@@ -1,23 +1,35 @@
 // Name: Leonard Paya, Bernardo Lin
 // Date: 2026-04-27
 // Functional unit for the VPE.
-// Performs one of seven operations on two 32-bit inputs based on opcode.
+// Performs one of seven operations on two signed INT8 inputs based on opcode.
 // Inputs:
-//     - in1: first 32-bit input
-//     - in2: second 32-bit input
+//     - in1: first 8-bit input
+//     - in2: second 8-bit input
 //     - opcode: selects the operation
 // Outputs:
 //     - out: result of the selected operation
 
 module fu(in1, in2, out, opcode);
-    input logic [31:0] in1, in2;
+    input logic [7:0] in1, in2;
     input logic [2:0] opcode;
-    output logic [31:0] out;
+    output logic [7:0] out;
 
-    logic signed [31:0] a;
-    logic signed [31:0] b;
-    logic signed [32:0] tmp;
-    logic signed [63:0] prod;
+    logic signed [7:0] a;
+    logic signed [7:0] b;
+    logic signed [15:0] tmp;
+    logic signed [15:0] prod;
+
+    function automatic logic signed [7:0] clamp_int8;
+        input logic signed [15:0] value;
+        begin
+            if (value > 16'sd127)
+                clamp_int8 = 8'sd127;
+            else if (value < -16'sd128)
+                clamp_int8 = -8'sd128;
+            else
+                clamp_int8 = value[7:0];
+        end
+    endfunction
 
     always_comb begin
 
@@ -28,14 +40,16 @@ module fu(in1, in2, out, opcode);
 
         case (opcode)
             3'b000: begin // ADD: add the two inputs
-                out = a + b;
+                tmp = a + b;
+                out = clamp_int8(tmp);
             end
             3'b001: begin // SUB: subtract second input from first
-                out = a - b;
+                tmp = a - b;
+                out = clamp_int8(tmp);
             end
-            3'b010: begin // MUL: multiply then shift right by 8 bits
+            3'b010: begin // MUL: multiply then shift right by 7 bits
                 prod = a * b;
-                out = prod >>> 8;
+                out = clamp_int8(prod >>> 7);
             end
             3'b011: begin // MAX: choose the larger input
                 if (a > b)
@@ -56,13 +70,15 @@ module fu(in1, in2, out, opcode);
                     out = in2;
             end
             3'b110: begin // ABS: output the absolute value of the first input
-                if (a[31])
+                if (a == -8'sd128)
+                    out = 8'sd127;
+                else if (a[7])
                     out = -a;
                 else
                     out = a;
             end
             default: begin
-                out = 32'h00000000;
+                out = 8'h00;
             end
         endcase
     end
