87b049c910a403370c46b9f374b1b5f1b3c010ff
kingsmanbl <bernardolinbl@gmail.com>
2026-05-07 16:26:52 -0700

    Parameterize VPU mux width

--- src/VectorProcessingUnit/mux2to1.sv
+++ src/VectorProcessingUnit/mux2to1.sv
@@ -1,17 +1,17 @@
 // Name: Leonard Paya, Bernardo Lin
 // Date: 2026-04-27
-// 32-bit two-input MUX. Selects one of two 32-bit inputs and forwards it to the output.
+// Parameterized two-input MUX. Selects one of two inputs and forwards it to the output.
 // Inputs:
 //     - d0: first data input
 //     - d1: second data input
 //     - select: choose d1 when high, otherwise choose d0
 // Outputs:
-//     - y: selected 32-bit output
+//     - y: selected output
 
-module mux2to1(d0, d1, select, y);
-	input logic [31:0] d0, d1;
+module mux2to1 #(parameter int BIT_WIDTH = 8) (d0, d1, select, y);
+	input logic [BIT_WIDTH - 1:0] d0, d1;
 	input logic select;
-	output logic [31:0] y;
+	output logic [BIT_WIDTH - 1:0] y;
 	
 	assign y = select ? d1 : d0;
 	
